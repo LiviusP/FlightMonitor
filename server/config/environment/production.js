85a24071f106b24038803053ba5ec4a8bdfe4e2e
Livius <plivius@yahoo.com>
2016-07-13 23:08:38 +0300

    Added backend host for confirmation maill

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,5 +19,7 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/flightmonitorbackend'
-  }
+  },
+
+  host: process.env.BACKEND_HOST || 'https://flightmonitorbackend.herokuapp.com'
 };
